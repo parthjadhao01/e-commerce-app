8fe60b199a27c4500c798082829a413af30951cc
Parth Jadhao <parthjadhao@Mac.lan>
2025-11-17 18:23:59 +0530

    fixed tailwind working

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import "./global.css";
-
-export default function index() {
+import "../global.css"
+import { Text, View } from "react-native";
+ 
+export default function App() {
   return (
-    <View>
-      <Text className='text-3xl'>index</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
-  )
+  );
 }
