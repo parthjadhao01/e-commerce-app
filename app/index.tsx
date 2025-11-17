f18b0f83ea8b4c3d4578bf369ccb937f9a0426f6
Parth Jadhao <parthjadhao@Mac.lan>
2025-11-17 17:59:06 +0530

    setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "./global.css";
+
+export default function index() {
+  return (
+    <View>
+      <Text className='text-3xl'>index</Text>
+    </View>
+  )
+}
